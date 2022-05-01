7e4e3601e1ed1125e8f664b6210c57870ee83a2e
Isaac Peña <isaac.pr99@gmail.com>
2022-05-01 02:46:04 -0400

    feat: apply new styles to <TagCollection/>

--- lib/celeb/TagCollection/index.tsx
+++ lib/celeb/TagCollection/index.tsx
@@ -1,25 +1,49 @@
-import React from 'react';
-import { useCelebContext } from '~/lib/components/StaticPropsContextProvider';
+import { Box, Center, Flex, HStack, Tag, Text, VStack } from "@chakra-ui/react";
+import React from "react";
+import { useCelebContext } from "~/lib/components/StaticPropsContextProvider";
 
 export const TagCollection = () => {
   const tags = useCelebContext().celeb.tags!;
 
   return (
-    <div style={{ backgroundColor: '#E8F8F5' }}>
-      <div>
+    <VStack
+      background={"gray.50"}
+      borderTop="1px solid #e5e7eb"
+      py={10}
+      gap={5}
+    >
+      <Center maxW={"container.lg"} flexWrap="wrap" gap={4}>
         {tags.regular.map((t) => (
-          <div key={t.tag.name}>{t.tag.name}</div>
+          <Tag
+            colorScheme={"gray"}
+            borderRadius="full"
+            variant={"subtle"}
+            size={"lg"}
+            key={t.tag.name}
+          >
+            {t.tag.name}
+          </Tag>
         ))}
-      </div>
+      </Center>
 
       {tags.lowConfidence.length > 0 && (
-        <div>
-          <p>Maybe</p>
-          {tags.lowConfidence.map((t) => (
-            <div key={t.tag.name}>{t.tag.name}</div>
-          ))}
-        </div>
+        <Center gap={2}>
+          <Text fontStyle={"italic"}>Maybe</Text>
+          <Center gap={4}>
+            {tags.lowConfidence.map((t) => (
+              <Tag
+                colorScheme={"gray"}
+                borderRadius="full"
+                variant={"outline"}
+                size={"lg"}
+                key={t.tag.name}
+              >
+                {t.tag.name}
+              </Tag>
+            ))}
+          </Center>
+        </Center>
       )}
-    </div>
+    </VStack>
   );
 };
