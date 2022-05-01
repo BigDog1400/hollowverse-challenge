54b7e40d265d575d1d88e562354d9a173a4dd6bb
Isaac Peña <isaac.pr99@gmail.com>
2022-05-01 08:25:24 -0400

    feat: add react-headroom to show or hide <AppBar/> when the user is scrolling up or down

--- lib/_app/AppBar/AppBar.tsx
+++ lib/_app/AppBar/AppBar.tsx
@@ -9,42 +9,45 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import { SearchIcon } from "@chakra-ui/icons";
+import Headroom from "react-headroom";
 
 export const AppBar = () => {
   return (
-    <Box background={"white"} boxShadow="sm" py={3}>
-      <Flex
-        as="section"
-        pos={"relative"}
-        justifyContent="space-around"
-        flexWrap={"wrap"}
-      >
-        <Box px={5}>
-          <Link passHref href="/">
-            <a>
-              <Image
-                src="/images/logo.svg"
-                width={250}
-                height={30}
-                alt="Hollowverse"
-                layout="fixed"
-              />
-            </a>
-          </Link>
-        </Box>
+    <Headroom>
+      <Box background={"white"} boxShadow="sm" py={3} zIndex={100}>
+        <Flex
+          as="section"
+          pos={"relative"}
+          justifyContent="space-around"
+          flexWrap={"wrap"}
+        >
+          <Box px={5}>
+            <Link passHref href="/">
+              <a>
+                <Image
+                  src="/images/logo.svg"
+                  width={250}
+                  height={30}
+                  alt="Hollowverse"
+                  layout="fixed"
+                />
+              </a>
+            </Link>
+          </Box>
 
-        <Box justifySelf={"center"} px={5}>
-          {/* Search input */}
-          <InputGroup variant={"outline"}>
-            <InputLeftElement
-              pointerEvents="none"
-              // eslint-disable-next-line react/no-children-prop
-              children={<SearchIcon color="gray.600" />}
-            />
-            <Input type="search" placeholder="Search for a celebrity" />
-          </InputGroup>
-        </Box>
-      </Flex>
-    </Box>
+          <Box justifySelf={"center"} px={5}>
+            {/* Search input */}
+            <InputGroup variant={"outline"}>
+              <InputLeftElement
+                pointerEvents="none"
+                // eslint-disable-next-line react/no-children-prop
+                children={<SearchIcon color="gray.600" />}
+              />
+              <Input type="search" placeholder="Search for a celebrity" />
+            </InputGroup>
+          </Box>
+        </Flex>
+      </Box>
+    </Headroom>
   );
 };
